8f1f342bb09fb5d984f32d1b484e311662ea76ec
Ricardo Gauto <ricardogauto11@gmail.com>
2021-02-04 13:13:07 -0300

    Pipes renderizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes';
+  name   : string = "rICaRdO gAuTO"
+  upper  : string = "Bulbasaur"
+  lower  : string = "Ivysaur"
+  slice  : string = "Feraligator"
+  PI     : number = Math.PI
+  percent: number = 0.4569
+  salary : number = 84500.50
+  dates  : Date = new Date()
+  poke = {
+    id   : 1,
+    name : 'Pikachu',
+    type : "Electric",
+    stats: {
+      hp     : 35,
+      attack : 55,
+      defense: 40,
+      speed  : 90
+    }
+  }
+  asynchronus = new Promise<string> ((res) => {
+    setTimeout(() => {
+      res('Iniciando')
+    }, 2500);
+  })
+  language : string = 'es'
+  videoUrl : string = 'https://www.youtube.com/embed/eMvn7VOGrxQ'
+  activated: boolean = true
 }
