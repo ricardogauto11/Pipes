2297822f96165e20b7a2e3bbd77b62b1d66d8d65
Ricardo Gauto <ricardogauto11@gmail.com>
2021-02-04 13:12:31 -0300

    Localize y Customs pipes agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import localEs from  '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr'
+
+registerLocaleData(localEs)
+registerLocaleData(localFr)
 
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+import { CapitalizedPipe } from './pipes/capitalized.pipe';
+import { SafeDOMPipe } from './pipes/safe-dom.pipe';
+import { HidenPipe } from './pipes/hiden.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizedPipe,
+    SafeDOMPipe,
+    HidenPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
